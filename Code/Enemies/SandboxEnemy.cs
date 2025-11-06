dedf8da549c408c3f2ebead671ea07e0b1f2349f
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-11-06 11:44:38 +0100

    characterbody

--- Code/Enemies/SandboxEnemy.cs
+++ Code/Enemies/SandboxEnemy.cs
@@ -2,10 +2,15 @@ using Godot;
 
 namespace Waves.Code.Enemies;
 
-public partial class SandboxEnemy : Area2D
+public partial class SandboxEnemy : CharacterBody2D
 {
+    [Export] private Area2D _area2D;
+
     public override void _Ready()
-        => BodyEntered += OnBodyEntered;
+    {
+        _area2D.BodyEntered += OnBodyEntered;
+        _area2D.AreaEntered += OnBodyEntered;
+    }
 
     private void OnBodyEntered(Node2D body)
     {
