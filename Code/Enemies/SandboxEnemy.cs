c144fea2e21afad525ffa50fbf4106de5a7a9265
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-11-06 14:29:20 +0100

    unique name

--- Code/Enemies/SandboxEnemy.cs
+++ Code/Enemies/SandboxEnemy.cs
@@ -5,8 +5,9 @@ namespace Waves.Code.Enemies;
 
 public partial class SandboxEnemy : CharacterBody2D
 {
-    [Export] private Area2D _area2D; // todo: dont export
+
     [Export] public float MoveSpeed = 120f;
+    private Area2D _area2D => GetNode<Area2D>(UniqueNames.Area2d);
     private Node2D _target;
 
     public override void _Ready()
---
+++ Code/Enemies/UniqueNames.cs
@@ -0,0 +1,6 @@
+namespace Waves.Code.Enemies;
+
+public static class UniqueNames
+{
+    public const string Area2d = "%Area2D";
+}
