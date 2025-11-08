a6fab998e6f4015801e04941cf555fb7434944f1
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-11-08 13:17:02 +0100

    pause on death

--- Code/SceneManagement/SceneControllers/SceneController.cs
+++ Code/SceneManagement/SceneControllers/SceneController.cs
@@ -1,7 +1,18 @@
 using Godot;
+using Waves.Code.Infrastructure;
 
 namespace Waves.Code.SceneManagement.SceneControllers;
 
 public partial class SceneController : Node
 {
+    public override void _Ready()
+        => EventBus.Instance.HitPointChanged += OnHitPointChanged;
+
+    private void OnHitPointChanged(int current, int max)
+    {
+        if (current <= 0)
+        {
+            GetTree().SetPause(true);
+        }
+    }
 }
