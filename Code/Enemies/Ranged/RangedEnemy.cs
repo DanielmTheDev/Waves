89aaac43da6af35db64841ad07eaa4f457fadbe5
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-11-15 19:13:37 +0100

    navigation mesh

--- Code/Enemies/Ranged/RangedEnemy.cs
+++ Code/Enemies/Ranged/RangedEnemy.cs
@@ -10,47 +10,76 @@ public partial class RangedEnemy : CharacterBody2D
     [Export] public Resources.RangedEnemyProfile Profile { get; set; }
 
     private Area2D _area2D => GetNode<Area2D>(UniqueNames.Area2d);
+    private NavigationAgent2D _agent => GetNode<NavigationAgent2D>(UniqueNames.NavigationAgent2d);
     private ProjectileShooter _shooter => GetNode<ProjectileShooter>(UniqueNames.ProjectileShooter);
+
     private Node2D _target;
 
     public override void _Ready()
     {
         AddToGroup(GroupNames.Enemy);
         _target = GetTree().GetFirstNodeInGroup(GroupNames.Player) as Node2D;
+        _agent.AvoidanceEnabled = true;
+        _agent.VelocityComputed += OnVelocityComputed;
+        _agent.DebugEnabled = true;
         _area2D.BodyEntered += OnBodyEntered;
         _area2D.AreaEntered += OnBodyEntered;
     }
 
     public override void _PhysicsProcess(double delta)
     {
+        // todo: this should look towards next path of agent
+        var toTarget = _target.GlobalPosition - GlobalPosition;
         Rotation = GlobalPosition.LookRotation(_target.GlobalPosition);
-        if (GlobalPosition.DistanceTo(_target.GlobalPosition) > Profile.ShootRange)
+
+        if (toTarget.Length() > Profile.ShootRange)
         {
-            MoveTowardsTarget();
+            UpdateAgentTarget();
+            SteerAlongPath();
         }
         else
         {
-            TryShoot();
+            Halt();
+            _shooter.TryShootAt(_target.GlobalPosition);
         }
     }
 
-    private void MoveTowardsTarget()
+    private void UpdateAgentTarget()
     {
-        var dir = GlobalPosition.NormalizedTo(_target.GlobalPosition);
-        Velocity = dir * Profile.MoveSpeed;
-        MoveAndSlide();
+        if (_agent.TargetPosition != _target.GlobalPosition)
+            _agent.TargetPosition = _target.GlobalPosition;
     }
 
-    private void TryShoot()
+    private void SteerAlongPath()
+    {
+        if (_agent.IsNavigationFinished())
+        {
+            Halt();
+            return;
+        }
+
+        _agent.TargetPosition = _target.GlobalPosition;
+
+        var next = _agent.GetNextPathPosition();
+        var desired = (next - GlobalPosition).Normalized() * Profile.MoveSpeed;
+        _agent.SetVelocity(desired);
+    }
+
+    private void Halt()
     {
         Velocity = Vector2.Zero;
         MoveAndSlide();
-        _shooter.TryShootAt(_target.GlobalPosition);
     }
 
     private void OnBodyEntered(Node2D body)
     {
         body.QueueFree();
         QueueFree();
+    }
+
+    private void OnVelocityComputed(Vector2 safeVelocity)
+    {
+        Velocity = safeVelocity;
+        MoveAndSlide();
     }
 }
--- Code/Enemies/Ranged/UniqueNames.cs
+++ Code/Enemies/Ranged/UniqueNames.cs
@@ -4,4 +4,5 @@ public static class UniqueNames
 {
     public const string Area2d = "%Area2D";
     public const string ProjectileShooter = "%ProjectileShooter";
+    public const string NavigationAgent2d = "%NavigationAgent2D";
 }
