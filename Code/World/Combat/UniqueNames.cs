b7f1232d565ac3b3d0962992f1d01af4708ae5b1
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-12-24 12:27:38 +0100

    WIP tomas

---
+++ Code/World/Combat/UniqueNames.cs
@@ -0,0 +1,7 @@
+namespace Waves.Code.World.Combat;
+
+public static class UniqueNames
+{
+    public const string HidingPoint = "%HidingPoint";
+    public const string ShootingPoint = "%ShootingPoint";
+}
