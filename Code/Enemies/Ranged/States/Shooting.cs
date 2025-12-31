513a5c0911be117cc404fabe80fd6eb3400099dd
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-12-31 17:46:08 +0100

    shoot for a time

--- Code/Enemies/Ranged/States/Shooting.cs
+++ Code/Enemies/Ranged/States/Shooting.cs
@@ -10,18 +10,21 @@ public class Shooting : State
     private readonly ProjectileShooter _shooter;
     private readonly Node2D _target;
     private readonly RangedEnemy _enemy;
+    private readonly RandomTimer _randomTimer;
 
     public Shooting(RangedEnemy enemy, Node2D target, ProjectileShooter shooter)
     {
         _shooter = shooter;
         _target = target;
         _enemy = enemy;
+        _randomTimer = new RandomTimer(3, 5);
     }
 
     public override void PhysicsProcess(double delta)
     {
+        _randomTimer.ElapseTime(delta);
         var distanceToTarget = _target.GlobalPosition.DistanceTo(_enemy.GlobalPosition);
-        if (distanceToTarget > _enemy.Profile.ShootRange)
+        if (distanceToTarget > _enemy.Profile.ShootRange || _randomTimer.IsDone())
         {
             _enemy.SwitchToTakingCover();
             return;
